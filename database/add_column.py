d32060015cd8c1c292522b44355c3a503db5602e
Emily Hunt <emily.hunt.physics@gmail.com>
2024-08-05 20:59:05 +0200

    Add authorized column to botactions

--- database/add_column.py
+++ database/add_column.py
@@ -1,7 +1,7 @@
 """Tool for adding a column(s) to the database."""
 
 from astrofeed_lib.database import db, Account, Post, SubscriptionState, BotActions, ModActions
-from playhouse.migrate import migrate, MySQLMigrator, IntegerField
+from playhouse.migrate import migrate, MySQLMigrator, IntegerField, BooleanField
 from playhouse.reflection import print_model
 
 
@@ -34,9 +34,26 @@ def migrate_2024_06_22():
     )
 
 
+def migrate_2024_08_05():
+    """Tasks for migration on 05/08/2024"""
+    migrator = MySQLMigrator(db)
+
+    # All new columns
+    authorized = BooleanField(null=False, index=True, default=True)
+
+    migrate(
+        # Add an index on DID - is stupid that there isn't one
+        # migrator.add_index("account", ("did",), unique=False),
+
+        # Add a moderator level field & index it
+        migrator.add_column("botactions", "authorized", authorized),
+    )
+
+
 if __name__ == "__main__":
     print_current_database_model()
     # migrate_2024_06_22()
+    migrate_2024_08_05()
     # print_current_database_model()
 
 
