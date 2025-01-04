62e0b286601239618f5ac046c0b906f45387abf8
Emily Hunt <emily.hunt.physics@gmail.com>
2025-01-04 21:39:07 +0100

    Update to work with new database systems

--- database/backup_database.py
+++ database/backup_database.py
@@ -1,22 +1,34 @@
 """Script to download a backup copy of the entire database."""
 
-from astrofeed_lib.database import db, Post, SubscriptionState, Account, BotActions, ModActions
+from astrofeed_lib.database import (
+    setup_connection,
+    teardown_connection,
+    get_database,
+    Post,
+    SubscriptionState,
+    Account,
+    BotActions,
+    ModActions,
+)
 from pathlib import Path
 import pandas as pd
 
 
 outdir = Path("../data/database_backup")
 
+
 # Sadly there's no easy way to do this with peewee...
 def download_all_posts(table):
     print(f"Downloading all posts for table {table.__name__}...")
     sql, params = table.select().sql()
-    dataframe = pd.read_sql_query(sql, db.connection(), params=params)
+    dataframe = pd.read_sql_query(sql, get_database().connection(), params=params)
     print("-> Saving!")
     if table.__name__ == "BotActions":
-        dataframe['checked_at'] = dataframe['checked_at'].astype(str)
+        dataframe["checked_at"] = dataframe["checked_at"].astype(str)
     dataframe.to_parquet(outdir / f"{table.__name__}.parquet")
 
 
 for table in (Post, SubscriptionState, Account, BotActions, ModActions):
+    setup_connection(get_database())
     download_all_posts(table)
+    teardown_connection(get_database())
